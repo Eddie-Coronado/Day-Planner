b338bd106065c85b7b11321a3f93776b3ff6fa70
Eddie-Coronado <75331399+Eddie-Coronado@users.noreply.github.com>
2021-03-11 20:44:57 -0800

    Adding Homework

---
+++ script.js
@@ -0,0 +1,14 @@
+// var currentHour = (new Date()).getHours();
+// $()
+//   .each(function(){
+//     var val = parseInt($(this).prop('id'));
+//     if(val > currentHour && val < currentHour + 6){
+//       $(this).css('background-color','Blue');
+//     }else if(val < currentHour && val > currentHour-6){
+//       $(this).css('background-color','Red');
+//     }else if(val === currentHour){
+//       $(this).css('background-color','Green');
+//     }else{
+//       $(this).css('background-color','White');
+//     }
+//   });
